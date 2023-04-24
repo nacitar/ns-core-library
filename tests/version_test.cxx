001bbb415da822659913fb81c5fa5d1b58fa2e0a
nacitar sevaht <nacitar.sevaht@gmail.com>
2023-04-24 01:48:33 -0600

    Git hash works correctly now.  Cmake scripts need split out.

--- tests/version_test.cxx
+++ tests/version_test.cxx
@@ -1,10 +1,11 @@
 #include <cstdio>
 
-#include "ns/core/version.h"
+#include "ns/core/version.hpp"
+#include "ns/core/vcs/metadata.hpp"
 
 int main() {
   printf("Project: %s\nVersion: %06X\nBranch: %s\nCommit Hash: %s\n",
       ns::core::kProjectName, ns::core::kVersion,
-      ns::core::kBranchName, ns::core::kCommitHash);
+      ns::core::vcs::kBranchName, ns::core::vcs::kCommitHash);
   return 0;
 }
