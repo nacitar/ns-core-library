469b5873910e7c2c38689d71a143f86937df5e04
nacitar sevaht <nacitar.sevaht@gmail.com>
2023-05-09 00:45:37 -0600

    First std header

--- tests/version_test.cxx
+++ tests/version_test.cxx
@@ -1,11 +1,12 @@
-#include <cstdio>
+#include <stdio.h>
 
 #include "ns/core/version.hpp"
 #include "ns/core/vcs/metadata.hpp"
 
 int main() {
   printf("Project: %s\n", ns::core::kProjectName);
-  printf("Version: %06X\n", ns::core::kVersion);
+  printf("Version: %02X%02X%02X\n",
+          ns::core::kVersionMajor, ns::core::kVersionMinor, ns::core::kVersionPatch);
   printf("Branch: %s\n", ns::core::vcs::kBranchName);
   printf("Commit Hash: %s\n", ns::core::vcs::kCommitHash);
   printf("Local Changes: %d\n", ns::core::vcs::kHasLocalChanges);
